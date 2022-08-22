ede42e12f2491624ce522641ca4f3a8e109ba6c9
Leandro Gonçalves <leandro.araujo_ext@p4f.com>
2022-08-22 14:41:35 -0300

    Send schedule to api

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -1,18 +1,18 @@
 import { Feather } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import { Alert, StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useTheme } from "styled-components";
-import AccelerationSvg from "../../assets/acceleration.svg";
-import ExchangeSvg from "../../assets/exchange.svg";
-import ForceSvg from "../../assets/force.svg";
-import GasolineSvg from "../../assets/gasoline.svg";
-import PeopleSvg from "../../assets/people.svg";
-import SpeedSvg from "../../assets/speed.svg";
 import { Accessory } from "../../components/Accessory";
 import { BackButton } from "../../components/BackButton";
 import { Button } from "../../components/Button";
 import { ImageSlider } from "../../components/ImageSlider";
+import { CarDTO } from "../../dtos/CarDTO";
+import { api } from "../../services/api";
+import { getAccessoriesIcon } from "../../utils/getAccessoriesIcon";
+import { getPlataformDate } from "../../utils/getPlataformDate";
 import {
   Accessories,
   Brand,
@@ -38,50 +38,87 @@ import {
   RentalPriceQuota,
   RentalPriceTotal,
 } from "./styles";
+interface Params {
+  car: CarDTO;
+  dates: string[];
+}
+
+interface RentalPeriodProps {
+  start: string;
+  end: string;
+}
 
 export function SchedulingDetails() {
+  const route = useRoute();
+  const [rentalPeriod, setRentalPeriod] = useState<RentalPeriodProps>(
+    {} as RentalPeriodProps
+  );
+  const { car, dates } = route.params as Params;
   const theme = useTheme();
 
   const navigation = useNavigation<any>();
+  const rentTotal = Number(dates.length * car.rent.price);
 
-  function handleConfirmRental() {
-    navigation.navigate("SchedulingComplete");
+  async function handleConfirmRental() {
+    const schedulesByCar = await api.get(`/schedules_bycars/${car.id}`);
+
+    const unavailable_dates = [...schedulesByCar.data.unavailable_dates, dates];
+
+    api
+      .put(`/schedules_bycars/${car.id}`, {
+        id: car.id,
+        unavailable_dates,
+      })
+      .then(() => navigation.navigate("SchedulingComplete"))
+      .catch(() => Alert.alert("Não foi possível confirmar o agendamento"));
   }
   function handleBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    setRentalPeriod({
+      start: format(getPlataformDate(new Date(dates[0])), "dd/MM/yyyy"),
+      end: format(
+        getPlataformDate(new Date(dates[dates.length - 1])),
+        "dd/MM/yyyy"
+      ),
+    });
+  }, []);
   return (
     <Container>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Header>
         <BackButton onPress={handleBack} />
       </Header>
       <CarImage>
-        <ImageSlider
-          imagesUrl={[
-            "https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png",
-          ]}
-        />
+        <ImageSlider imagesUrl={car.photos} />
       </CarImage>
 
       <Content>
         <Details>
           <Description>
-            <Brand>Lamborguine</Brand>
-            <Name>Huracan</Name>
+            <Brand>{car.brand}</Brand>
+            <Name>{car.name}</Name>
           </Description>
           <Rent>
-            <Period>Ao dia</Period>
-            <Price>R$ 580</Price>
+            <Period>{car.rent.period}</Period>
+            <Price>{`R$ ${car.rent.price}`}</Price>
           </Rent>
         </Details>
 
         <Accessories>
-          <Accessory icon={SpeedSvg} name="380km/h" />
-          <Accessory icon={AccelerationSvg} name="3.2s" />
-          <Accessory icon={ForceSvg} name="800 HP" />
-          <Accessory icon={GasolineSvg} name="Gasolina" />
-          <Accessory icon={ExchangeSvg} name="Auto" />
-          <Accessory icon={PeopleSvg} name="2 pessoas" />
+          {car.accessories.map((accessory) => (
+            <Accessory
+              key={accessory.type}
+              name={accessory.name}
+              icon={getAccessoriesIcon(accessory.type)}
+            />
+          ))}
         </Accessories>
         <RentalPeriod>
           <CalendarIcon>
@@ -93,7 +130,7 @@ export function SchedulingDetails() {
           </CalendarIcon>
           <DateInfo>
             <DateTitle>DE</DateTitle>
-            <DateValue>18/06/2021</DateValue>
+            <DateValue>{rentalPeriod.start}</DateValue>
           </DateInfo>
           <Feather
             name="chevron-right"
@@ -101,15 +138,17 @@ export function SchedulingDetails() {
             color={theme.colors.text}
           />
           <DateInfo>
-            <DateTitle>DE</DateTitle>
-            <DateValue>18/06/2021</DateValue>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue>{rentalPeriod.end}</DateValue>
           </DateInfo>
         </RentalPeriod>
         <RentalPrice>
           <RentalPriceLabel>TOTAL</RentalPriceLabel>
           <RentalPriceDetail>
-            <RentalPriceQuota>R$ 580 x 3 diárias</RentalPriceQuota>
-            <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+            <RentalPriceQuota>{`R$ ${car.rent.price} x ${Number(
+              dates.length
+            )} diárias`}</RentalPriceQuota>
+            <RentalPriceTotal>{`R$ ${rentTotal}`}</RentalPriceTotal>
           </RentalPriceDetail>
         </RentalPrice>
       </Content>
--- src/utils/getPlataformDate.ts
+++ src/utils/getPlataformDate.ts
@@ -3,7 +3,7 @@ import { addDays } from "date-fns";
 import { Platform } from "react-native";
 
 export function getPlataformDate(date: Date) {
-  if (Platform.OS === "android") {
+  if (Platform.OS === "android" || Platform.OS === "ios") {
     return addDays(date, 1);
   } else {
     return date;
